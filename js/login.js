8ce370026ff8450d4706d9255654196946b2ba10
carifossti <cfossati@internet.com.uy>
2021-08-17 23:20:28 -0300

    nuevo respaldo

--- js/login.js
+++ js/login.js
@@ -1,6 +1,38 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
+document.addEventListener("DOMContentLoaded", function(e){
 });
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId());
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log ("ID Token: " + id_token);
+  }
+
+    function verificar(){
+        let user = document.getElementById("user");
+        let pass = document.getElementById("pass");
+        let msj = document.getElementById ("msj");
+        let usuario = {};
+       
+        if ( user.value.trim() ==='' || pass.value.trim()==='' ){
+
+            user.classList.add("notValid"); 
+            msj.innerHTML="Dato requerido";
+            msj.classList.add("notValid");            
+        }
+        else{
+            
+            location.href="index.html";
+
+            usuario.nombre = user.value;
+            usuario.estado = "Conectado";
+
+            localStorage.setItem('usuario',JSON.stringify(usuario)); //guardo mi variable de objeto en local storage
+            sessionStorage.setItem('usuario',JSON.stringify(usuario));
+        }
+    }
